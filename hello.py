01f039c37648e05fc6963fd18842265faed1175d
kenneally15 <kenneally15@yahoo.com>
2023-02-10 17:16:49 -0500

    Added Features

--- hello.py
+++ hello.py
@@ -4,6 +4,21 @@ msg = "Hello World"
 print(msg)
 
 msg = "New message"
-print(msg)
+print(msg,"!!!")
+
+x = 5
+y = 10
+
+for i in range(x):
+    y += 10
+
+print(y)
+
+msg2 = "Second message"
+print(msg2)
+
+def add(x,y):
+    return x+y
 
 
+print(add(3,9))
